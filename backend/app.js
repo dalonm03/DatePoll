e2f0149955a393db796a18154dea47b362dbeecd
Jgarcg40 <Jgarcg40@estudiantes.unileon.es>
2020-06-05 01:56:04 +0200

    añadidas las tablas a sequelice

--- backend/app.js
+++ backend/app.js
@@ -24,3 +24,71 @@ sequelize
   .catch(err => {
     console.error("Unable to connect to the database:", err);
   });
+
+
+  class encuesta extends Sequelize.Model {}
+  encuesta.init(
+  {
+    idencuesta: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    nombrencuesta: Sequelize.STRING,
+    fechainicio: Sequelize.DATE,
+    fechafinal: Sequelize.DATE,
+    descrpcion: Sequelize.STRING,
+    mes: Sequelize.INTEGER,
+    
+   
+  },
+  { sequelize, modelName: "encuesta" }
+);
+
+  class respuesta extends Sequelize.Model {}
+  respuesta.init(
+  {
+    idrespuesta: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    fecharespuesta: Sequelize.DATE,  
+    nombre: Sequelize.STRING,
+  
+  },
+  { sequelize, modelName: "respuesta" }
+);
+
+class usuarios extends Sequelize.Model {}
+usuarios.init(
+{
+  idusuariosa: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+
+  nombre: Sequelize.STRING,
+  apellido1: Sequelize.STRING,
+  apellido2: Sequelize.STRING,
+  
+ 
+},
+{ sequelize, modelName: "usuarios" }
+);
+
+
+
+sequelize.sync();
+
+app.post("/getContactos", function(req, res) {
+  sequelize
+    .query(
+      "SELECT idrespuesta FROM contactos",
+      { type: sequelize.QueryTypes.SELECT }
+    )
+    .then(contacts => {
+      res.send(contacts);
+    });
+});
